e721a7f16b55fd3aa4d003e38ec88ce0c066a4ee
jschaef80 <jschaef80@gmail.com>
2019-05-09 20:22:06 -0500

    turning in module practice

---
+++ src/modules/subtractNumbers.js
@@ -0,0 +1,19 @@
+import subtract from "./subtract";
+
+export function subtractTen(num) {
+  return subtract(num,10);
+}
+
+export function subtractFive(num) {
+    return subtract(num, 5);
+  }
+
+export function subtractTwo(num) {
+    return subtract(num, 2);
+}
+
+export function subtractOne(num) {
+  return subtract(num, 1);
+}
+
+export default subtractTwo;
